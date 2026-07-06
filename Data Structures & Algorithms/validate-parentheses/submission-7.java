21dbb8f868ca509ddeb9bc15ce73661eaf34603d
Szczepan Kurnyta <corvetta77@users.noreply.github.com>
2026-07-06 18:55:36 +0200

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        
+        Map<Character, Character> parenthesesMapping = new HashMap<>();
+        parenthesesMapping.put(']', '[');
+        parenthesesMapping.put('}', '{');
+        parenthesesMapping.put(')', '(');
+    
+        for (char currChar : s.toCharArray()) {
+            if (parenthesesMapping.containsKey(currChar)) {
+                if (!stack.isEmpty() && stack.peek() == (parenthesesMapping.get(currChar))) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(currChar);
+            }
+        }
+
+       return stack.isEmpty();
+    }
+}
